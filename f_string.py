df6507adb66f7e1ae89faa892750766a3c9389e2
Nathalia Gutierrez Pinilla <156914349+Natha91-z@users.noreply.github.com>
2026-01-17 03:53:01 -0500

    Add files via upload

---
+++ f_string.py
@@ -0,0 +1,15 @@
+name = 'Eduardo'
+last_name = 'Garcia'
+
+full_name = f'El nombre completo es: {"Eduardo Ismael"} {last_name} Su edad es: {30}'
+print(full_name)
+
+
+print(title.upper())
+print(title.lower())
+
+print(title.strip())
+print(title.find('p'))
+print('100'.isnumeric())
+
+
--- nuevo_streaans.py
+++ nuevo_streaans.py
@@ -1,12 +1,23 @@
 name = "eduardo"
-las_name = "Gracia"
+last_name = "Gracia"
 
-full_name = name + " " + las_name
+full_name = name + " " + last_name
 print(full_name)
 
 
-full_name = ' '.join([name, las_name])
+full_name = ' '.join([name, last_name])
 print(full_name)    
 
-full_name '%s %s' % (name, las_name)
+full_name  = 'El nombre completo es: %s %s'  %(name, last_name)
 print(full_name)
+
+base = 'El nombre completo es: {} {}. Se edad es: {}'
+full_name =  base.format(name, last_name, 30)
+print(full_name)    
+
+name = input("Ingresa tu nombre: ")
+last_name = input("Ingresa tu apellido: ")  
+age = input("Ingresa tu edad: ") 
+
+print(full_name)
+
